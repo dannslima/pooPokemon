3dbf0ef934a2fa46f7f8790892e49bbfb1908906
DANILO LIMA <danilo.lima56@fatec.sp.gov.br>
2021-12-23 10:24:01 -0300

    Adicionar arquivos de projeto.

---
+++ Pokemon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemon
+{
+    public class Pokemon
+    {
+        private string nome;
+
+        public string _Nome
+        {
+            get { return nome; }
+            set {
+                String texto = value.ToUpper();
+                nome = texto;
+            }
+        }
+
+        private String descricao; //ARMAZENA O VALOR DA PROPRIEDADE DESCRICAO
+
+        public String _Descricao // PROPRIEDADE REPRESENTA A CARACTERISTICA DESCRICAO DO MEU POKEMON
+        {
+            get { return this.descricao; }
+            set { this.descricao = value; }
+        }
+
+        public void ExibirDadosPokemon()
+        {
+
+        }
+
+    }
+}
